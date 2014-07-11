8159b10537c0784c93ac66bcfefbcb9d5f5b4fa5
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-07-10 20:01:20 -0700

    continuing up to ch3 excercizes

--- payments/tests.py
+++ payments/tests.py
@@ -27,6 +27,15 @@ class UserModelTest(TestCase):
 
     def test_get_by_id(self):
         self.assertEqual(User.get_by_id(1), self.test_user)
+        
+    def test_create_user_function_stores_in_database(self):
+        user = User.create("test", "test@t.com", "tt", "1234", "22")
+        self.assertEqual(User.objects.get(email="test@t.com"), user)
+        
+    def test_create_user_already_exists_throws_IntegrityError(self):
+        from django.db import IntegrityError
+        self.assertRaises(IntegrityError, User.create, "test user", "j@j.com",
+                          "jj", "1234", 89)
 
 # ---------- FORMS TESTS ---------- #
 class FormTesterMixin():
@@ -161,6 +170,123 @@ class RegisterPageTests(TestCase, ViewTesterMixin):
                                         register,
                                         html.content,
                                         )
+
+    def setUp(self):
+        request_factory = RequestFactory()
+        self.request = request_factory.get(self.url)
+        
+    def test_invalid_form_returns_registration_page(self):
+        
+        with mock.patch('payments.forms.UserForm.is_valid') as user_mock:
+            
+            user_mock.return_value = False
+            
+            self.request.method = 'POST'
+            self.request.POST = None
+            resp = register(self.request)
+            self.assertEqual(resp.content, self.expected_html)
+            
+            #make sure that we did indeed call our is_valid funciton
+            self.assertEqual(user_mock.call_count, 1)
+    
+    @mock.patch('stripe.Customer.create')
+    @mock.patch(User, 'create')
+    def test_registering_new_user_returns_successfully(self, create_mock,
+                                                       stripe_mock):
+        
+        self.request.session = {}
+        self.request.method = 'POST'
+        self.request.POST = {'email' : 'python@rocks.com',
+                             'name' : 'pyRock',
+                             'stripe_token' : '...',
+                             'last_4_digits' : '4242',
+                             'password' : 'bad_password',
+                             'ver_password' : 'bad_password',
+                             }
+        
+        #get the return values of the mocks, for our checks later
+        new_user = create_mock.return_value
+        new_cust = stripe_mock.return_value
+        
+        resp = register(self.request)
+        
+        self.assertEqual(resp.content, "")
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(self.request.session['user'], new_user.pk)
+        #verify the user was actually stored in the db.
+        
+        create_mock.assert_called_with('pyRock', 'python@rocks.com',
+                                       'bad_password', '4242', new_cust.id)
+        
+        #with mock.patch('stripe.Customer') as stripe_mock:
+        #    
+        #    config = {'create.return_value': mock.Mock()}
+        #    stripe_mock.configure_mock(**config)
+        #    
+        #    resp = register(self.request)
+        #    self.assertEqual(resp.content, "")
+        #    self.assertEqual(resp.status_code, 302)
+        #    self.assertEqual(self.request.session['user'], 1)
+        #    
+        #    #verify the user was actually stored in the database.
+        #    #if not, this will throw an error
+        #    db_user = User.objects.get(email='python@rocks.com')
+        
+        #resp = register(self.request)
+        #self.assertEqual(resp.status_code, 200)
+        #self.assertEqual(self.request.session, {})
+        
+    def test_registering_user_twice_cause_error_msg(self):
+        
+        #create a user with the same email so we get an integrity error
+        user = User(name='pyRock', email='python@rocks.com')
+        user.save()
+        
+        #now create the request used to test the view
+        self.request.session = {}
+        self.request.method = 'POST'
+        self.request.POST = {'email' : 'python@rocks.com',
+                             'name' : 'pyRock',
+                             'stripe_token' : '...',
+                             'last_4_digits' : '4242',
+                             'password' : 'bad_password',
+                             'ver_password' : 'bad_password',    
+                            }
+        
+        #create our expected form
+        expected_form = UserForm(self.request.POST)
+        expected_form.is_valid()
+        expected_form.addError('python@rocks.com is already a member')
+        
+        #create the expected html
+        html = render_to_response('register.html',
+                                  {'form': expected_form,
+                                   'months': range(1, 12),
+                                   'publishable': settings.STRIPE_PUBLISHABLE,
+                                   'soon': soon(),
+                                   'user': None,
+                                   'years': range(2011, 2036),
+                                   })
+        
+        #mock out stripe so we don't hit their server
+        with mock.patch('stripe.Customer') as stripe_mock:
+            
+            config = {'create.return_value': mock.Mock()}
+            stripe_mock.configure_mock(**config)
+            
+            #run the test
+            resp = register(self.request)
+            
+            #verify that we did things correctly
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(self.request.session, {})
+            
+            #assert there is only one record in the database
+            users = User.objects.filter(email="python@rocks.com")
+            self.assertEqual(len(users), 1)
+            
+            #verify HTML being returned is correct
+            self.assertEqual(resp.content, html.content)
 
 class EditPageTests(TestCase, ViewTesterMixin):
 
