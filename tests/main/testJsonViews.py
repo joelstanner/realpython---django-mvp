31ff539ab0b0bfeada2cf33859e029f58957b05a
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-08-06 02:26:24 -0700

    up to authentication

--- tests/main/testJsonViews.py
+++ tests/main/testJsonViews.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from main.models import StatusReport
 from main.serializers import StatusReportSerializer
-from main.json_views import status_collection
+from main.json_views import StatusCollection
 from payments.models import User
 from django.core import serializers
 from rest_framework import status
@@ -30,12 +30,12 @@ class JsonViewTests(TestCase):
     def test_get_collection(self):
         status = StatusReport.objects.all()
         expected_json = StatusReportSerializer(status, many=True).data
-        response = status_collection(dummyRequest('GET'))
+        response = StatusCollection.as_view()(dummyRequest('GET'))
         
         self.assertEqual(expected_json, response.data)
         
     def test_get_collection_requires_auth(self):
         anon_request = dummyRequest("GET", authed=False)
-        response = status_collection(anon_request)
+        response = StatusCollection.as_view()(anon_request)
         
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
